30cbf050af8a4a47930845e23a4fee1c5a7fb960
Saleem Ullah <saleemhafizj@gmail.com>
2026-01-15 13:41:42 +0500

    loops

---
+++ 20260115/02_js-loops.js
@@ -0,0 +1,18 @@
+// For loop
+for (let i = 1; i <= 5; i++) {
+  console.log("For loop count:", i);
+}
+
+// While loop
+let j = 1;
+while (j <= 5) {
+  console.log("While loop count:", j);
+  j++;
+}
+
+// Do-while loop
+let k = 1;
+do {
+  console.log("Do-while loop count:", k);
+  k++;
+} while (k <= 5);
